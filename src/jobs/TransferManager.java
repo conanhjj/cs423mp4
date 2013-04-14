a662e872bec1e14347f8534c92781688f337cb28
Raviphol Sukhajoti <raviphol@gmail.com>
2013-04-13 20:12:36 -0500

    Add base code.

--- src/jobs/TransferManager.java
+++ src/jobs/TransferManager.java
@@ -1,5 +1,83 @@
 package jobs;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/*
+ * Transfer Manager: The Transfer Manager is responsible of performing a load transfer 
+ * upon request of the adaptor. It must move jobs from the Job Queue and send them to remote node. 
+ * It must also receive any jobs sent by the remote node and place them in the local Job Queue. 
+ * You can use any protocol that you choose (e.g. TCP, UDP, HTTP, etc.)
+ */
 
 public class TransferManager {
+	public Socket socket;
+	public String hostname;
+	public Listener listener;
+	public int port;
+	
+	public TransferManager(){
+		hostname = "10.10.10.1";
+		port = 4567;
+
+		try {    
+			socket = new Socket(hostname, port);
+			listener = new Listener(this);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public void sendJob(Job job){
+		// remove from job queue
+		try 
+        {
+            ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
+            objectOutput.writeObject(job);                
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        } 
+	}
+	
+	public void receiveJob(Job job){
+		// put into job queue
+	}
+	
+	/*
+	 * ref: http://stackoverflow.com/questions/959785/java-sockets-asynchronous-wait-synchronous-read
+	 */
+	public class Listener extends Thread {
+		TransferManager transferManager;
+		public Listener(TransferManager tm) {
+
+		    // initialize thread resources (sockets, database connections, etc)
+			this.transferManager = tm;
+		    start();
+		    Thread.yield();
+		}
+
+		public void run() {
+			while(true)	{
+				// read message from socket;
+				try {
+					ObjectInputStream objectInput = new ObjectInputStream(transferManager.socket.getInputStream());
+	                try {
+	                    Job job = (Job) objectInput.readObject();
+	                    transferManager.receiveJob(job);
+	                } catch (ClassNotFoundException e) {
+	                    e.printStackTrace();
+	                }
+				} catch (IOException e) {
+					e.printStackTrace();
+				}  
+			}
+		}
+	}
 }
--- src/state/HardwareMonitor.java
+++ src/state/HardwareMonitor.java
@@ -1,5 +1,42 @@
 package state;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+import com.sun.management.OperatingSystemMXBean;
+
 
 public class HardwareMonitor {
+	/*
+	 * ref: http://stackoverflow.com/questions/5907519/measure-cpu-usage-of-the-jvm-java-code
+	 */
+	public static double getCpuUtilzation(){
+		
+		OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+	    RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+	    int availableProcessors = operatingSystemMXBean.getAvailableProcessors();
+	    long prevUpTime = runtimeMXBean.getUptime();
+	    long prevProcessCpuTime = operatingSystemMXBean.getProcessCpuTime();
+	    double cpuUsage;
+	    
+	    // Throtling ???
+	    try 
+	    {
+	        Thread.sleep(500);
+	    } 
+	    catch (Exception ignored) { }
+
+	    operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+	    long upTime = runtimeMXBean.getUptime();
+	    long processCpuTime = operatingSystemMXBean.getProcessCpuTime();
+	    long elapsedCpu = processCpuTime - prevProcessCpuTime;
+	    long elapsedTime = upTime - prevUpTime;
+
+	    cpuUsage = Math.min(99F, elapsedCpu / (elapsedTime * 10000F * availableProcessors));
+	    return cpuUsage;
+	}
+	
+	public static void main(String [] args) {
+		System.out.println(getCpuUtilzation());
+	}
 }
