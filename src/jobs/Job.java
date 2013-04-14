0c5b8199b796f20c09ca5241b165649b93a5b9f5
conanhjj <conanhjj@gmail.com>
2013-04-13 19:57:41 -0500

    Add base code.

--- src/jobs/Job.java
+++ src/jobs/Job.java
@@ -2,19 +2,25 @@ package jobs;
 
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class Job implements Serializable {
 
     private JobResult result;
     private String executeCommand;
 
+    private String fileName;
+    private String binaryCode;
+
+    private UUID jobId;
+
     public Job() {
         this("null");
     }
 
-    public Job(String cmd) {
-        result = new JobResult();
-        executeCommand = cmd;
+    public Job(String fileName) {
+        this.fileName = fileName;
+        jobId = UUID.randomUUID();
     }
 
     public String getCommand() {
--- src/jobs/WorkerThread.java
+++ src/jobs/WorkerThread.java
@@ -3,6 +3,13 @@ package jobs;
 
 import util.Util;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
 public class WorkerThread {
 
     private Thread thread;
@@ -10,7 +17,8 @@ public class WorkerThread {
 
     private boolean stopWork;
 
-    private static Long SLEEP_INTERVAL = 2000L;
+    private static Long NO_JOB_SLEEP_INTERVAL = 2000L;
+    private static Long FINISH_SLEEP_INTERVAL = 1000L;
 
     public WorkerThread() {
         jobQueue = new JobQueue();
@@ -23,12 +31,13 @@ public class WorkerThread {
             public void run() {
                 while(!stopWork) {
                     if(jobQueue.isEmpty()) {
-                        Util.sleep(SLEEP_INTERVAL);
+                        Util.sleep(NO_JOB_SLEEP_INTERVAL);
                         continue;
                     }
 
                     Job job = jobQueue.pop();
                     executeJob(job);
+                    Util.sleep(FINISH_SLEEP_INTERVAL);
                 }
             }
         });
@@ -42,7 +51,39 @@ public class WorkerThread {
 
     public void executeJob(Job job) {
         //TODO: fake code
+
+
         Util.sleep(2000);
+    }
+
+    private void executeJob(String command) {
+        try {
+            List<String> cmds = new LinkedList<String>();
+            cmds.add("sh");
+            cmds.add("-c");
+            cmds.add(command);
+
+            ProcessBuilder pb = new ProcessBuilder(cmds);
+            Process p = pb.start();
+            BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(p.getInputStream())));
+
+            String lineStr;
+
+            while((lineStr = br.readLine()) != null) {
+                System.out.println(lineStr);
+            }
+
+            if(p.waitFor() != 0) {
+                if(p.exitValue() == 1) {
+                    System.err.println("Job Execute error");
+                }
+            }
+
+        } catch (IOException ex) {
+            System.err.println(ex);
+        } catch (InterruptedException ex) {
+            System.err.println(ex);
+        }
     }
 
     public void addJob(Job job) {
